e59865a1af3c31b01d9088308584aa29ee6d45c5
roberto <robertorebolledo151@gmail.com>
2022-10-24 14:28:35 +0200

    cosas

---
+++ Programación/EjemplosTema2/src/condosbucles/Principal.java
@@ -0,0 +1,26 @@
+package condosbucles;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int tope1=3, tope2=3,num=0;
+		
+		for (int i=0 ; i<tope1 ; i++) {
+			System.out.println("");
+			
+			for (int j=0; j<tope2;j++) {
+				num++;
+				System.out.print(" "+num);
+				
+			}
+			
+		}
+		
+		
+		
+		
+		
+	}
+
+}
