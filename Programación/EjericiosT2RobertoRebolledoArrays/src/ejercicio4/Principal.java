0e98979d47c980bb59a77c9b7de187fe1fdeec8c
Roberto <robertorebolledo151@gmail.com>
2022-11-02 23:58:29 +0100

    programación

--- Programación/EjericiosT2RobertoRebolledoArrays/src/ejercicio4/Principal.java
+++ Programación/EjericiosT2RobertoRebolledoArrays/src/ejercicio4/Principal.java
@@ -9,35 +9,55 @@ public class Principal {
 	public static void main(String[] args) {
 		/*Rellenar un array unidimensional de 10 posiciones con números aleatorios entre 1 y 99 y mostrar el 
 		 * número guardado en un lugar que indique el usuario por teclado.*/
-		int tam=10, num1,max=99,min=1;
-		int [] posicion;
-		int [] orden;
-		Random num=new Random(System.nanoTime());
-		System.out.println("Bienvenidos al programa");
-		posicion = new int [tam];
-		orden = new int [tam];
+		int tam=10, max=99, min=1,selec,bucle;
+		int num[];
+		Random r= new Random (System.nanoTime() );
+		System.out.println("Bienvenido al programa que guarda 10 números aleatorios");
+		num = new int [tam];
 		
-		for (int i = 0; i < orden.length; i++) {
-			orden[i]=0;
-			System.out.println(orden[i]);
-		}
-		
-		for (int i = 0; i < posicion.length; i++) {
-			System.out.println("¿Dódnde desea guardar el "+(1+i)+"º número?");
-			num1=Leer.datoInt();
-			if(orden[num1]!=0){
+		do {
+			System.out.println("Seleccione una opción");
+			System.out.println("1.-Generar números aleatorios");
+			System.out.println("2.-Mostrar los números");
+			System.out.println("0.-Salir");
+			bucle=Leer.datoInt();
+			
+			switch(bucle) {
+			
+				case 1:
+					for (int i = 0; i < num.length; i++) {
+						
+						num[i]=r.nextInt(max-min)+min;
+			
+					}
+					System.out.println("Números generados");
+					
+					break;
+			
+				case 2:
+					do{
+						System.out.println("Selecciona la posición del número qwue se encuentra entre el "+min+" o el "+max+" o seleccione 0 para salir");
+						selec=Leer.datoInt();
+						System.out.println("Ha seleccionado la posición número "+selec);
+						System.out.println(num[selec-1]);
+						
+					}while(selec!=0);
+					break;
+					
+					
+				case 0:
+					break;
 				
-				System.out.println("Este espacio del array ya ha sido utilizado, por favor utilize otro.");
-				i=i-1;
-				
-			}else {
-			posicion[num1]=num.nextInt(max-min)+min;
-			System.out.println(posicion[num1]);
+				default:
+					System.out.println("No disponible, por favor selecciona una opción disponible");
+					break;
+					
+					
 			}
-
-		}
+			
 		
-		
+		}while(bucle!=0);
+		System.out.println("Gracias por usar el programa");
 		
 	}
 
---
+++ Programación/EjericiosT2RobertoRebolledoArrays/src/ejercicio5/Principal.java
@@ -0,0 +1,88 @@
+package ejercicio5;
+
+import java.util.Random;
+
+import utilidades.Leer;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		/*Crear un programa que realice lo siguiente:
+			• Solicitar al usuario que introduzca por teclado el tamaño del array.
+			• Declarar y definir el array de enteros.
+			• Rellenar el array mediante números aleatorios entre los dos valores que diga el usuario.
+			• Mostrar por pantalla la suma de todos los elementos.
+			• Mostrar el mayor y el menor guardados en el array.
+			• Modificar un valor elegido por el usuario.*/
+		
+		int tam,max,min,suma=0,men=0,may=0,sino,cambio,nuevo;
+		int num[];
+		Random r= new Random (System.currentTimeMillis());
+		System.out.println("Bienvenidos al progarma");
+		
+		System.out.println("Introduzca el tamaño de los números a almacenar");
+		tam=Leer.datoInt();
+		num= new int[tam];
+		
+		System.out.println("Seleccione el número máximo que quieras que pueda generar");
+		max=Leer.datoInt();
+		
+		System.out.println("Seleccione el número mínimo que quieras que pueda generar");
+		min=Leer.datoInt();
+		
+		for (int i = 0; i < num.length; i++) {
+			num[i]=r.nextInt(max-min)+min;
+		}
+		
+		for (int i = 0; i < num.length; i++) {
+			suma+=num[i];
+		}
+		
+		System.out.println("La suma de todos los números es de "+suma);
+		
+		for (int i = 0; i < num.length; i++) {
+		
+			if(may<num[i]) {
+				
+				may=num[i];
+				
+			}
+			
+			if(num[i]>num[i+1]) {
+				
+				men=num[i+1];
+				
+			}
+		
+		
+		}
+		System.out.println("El número mayor es "+may+" y el menor es "+men);
+		
+
+		System.out.println("Quiere cambiar algún núemro");
+		System.out.println("1.- Si");
+		System.out.println("2.- No");
+		sino=Leer.datoInt();
+		if(sino==1) {
+			do {
+
+					
+				System.out.println("¿Qúe valor desea cambiar?");
+				cambio=Leer.datoInt();
+				System.out.println("¿Por qué valor quiere cambiarlo?");
+				nuevo=Leer.datoInt();
+				
+				num[cambio-1]=nuevo;
+				System.out.println("Quiere cambiar algún núemro");
+				System.out.println("1.- Si");
+				System.out.println("2.- No");
+				sino=Leer.datoInt();
+					
+					
+			}while(sino==1);
+		}
+		
+		
+	}
+
+}
