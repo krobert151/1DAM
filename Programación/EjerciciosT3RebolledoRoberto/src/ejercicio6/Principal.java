06c3507722cb166d63fbab1ad7d723a14f376cab
roberto <robertorebolledo151@gmail.com>
2022-11-28 14:22:15 +0100

    cosas

--- Programación/EjerciciosT3RebolledoRoberto/src/ejercicio6/Principal.java
+++ Programación/EjerciciosT3RebolledoRoberto/src/ejercicio6/Principal.java
@@ -1,8 +1,11 @@
 package ejercicio6;
 
+import utilidades.Leer;
+
 public class Principal {
 
 	public static void main(String[] args) {
+		int num;
 		/*Crear un programa que simule el juego de cara o cruz. Para ello, se usarán los siguientes métodos:
 		• Método que simule el lanzamiento de la moneda y resultado.
 		• Método para comprobar ganador, se le pasará el número elegido por el jugador y dentro se generará
@@ -11,6 +14,17 @@ public class Principal {
 			si no, debe mostrar "Siga jugando".
 		• En el main, se debe crear lo necesario para simular un juego de cara o cruz.*/
 
+		System.out.println("Bienvenido a cara o cuz");
+		System.out.println("\nSeleciona cara o cruz");
+		System.out.println("1.-Cara");
+		System.out.println("2.-Cruz");
+		num=Leer.datoInt();
+		while(num<0|num>2) {
+			System.out.println("Número no valido");
+			num=Leer.datoInt();
+		}
+		
+		
 		
 		
 		
